63dc025b4d7c2785ae9cbf8a0967e6fb972a1a5b
Jason Horn <jasonh.horn1@gmail.com>
2021-02-05 20:06:43 -0800

    schema

--- schema.sql
+++ schema.sql
@@ -2,3 +2,34 @@ DROP DATABASE IF EXISTS real_estate_listings;
 
 CREATE DATABASE real_estate_listings;
 
+CREATE TABLE listings (
+  id serial primary key,
+  tags,
+  price,
+  line1,
+  line2,
+  neighborhood,
+  neighborhoodUrl,
+  numBeds,
+  numBaths,
+  sqft,
+  images,
+  views
+);
+
+CREATE TABLE users (
+  id serial primary key,
+  username varchar(80),
+  pword varchar(80),
+  first_name varchar(80),
+  last_name varchar(80),
+  email varchar(80),
+);
+
+CREATE TABLE favorites (
+
+)
+
+CREATE TABLE images (
+
+)
